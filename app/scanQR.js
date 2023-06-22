62df646d5310378b25d4b2ed4217c2e191fcdaef
Vivian-Trieu <vivian.dtrieu@gmail.com>
2023-06-21 21:55:10 -0700

    Navigation to another screen

---
+++ app/scanQR.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const scanQR = () => {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>scanQR</Text>
+    </View>
+  );
+};
+
+export default scanQR;
