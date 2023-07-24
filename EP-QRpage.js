32283a12d9fd9fa07686c007d7367aff14056801
jasroop-singh <113493289+jasroop-singh@users.noreply.github.com>
2023-07-23 23:54:46 -0700

    Add files via upload

---
+++ EP-QRpage.js
@@ -0,0 +1,23 @@
+function RequestBevCart() {
+    var chino_btn = document.getElementById('chino');
+    var butter_btn = document.getElementById('butter');
+
+    chino_btn.classList.remove('hidden');
+    butter_btn.classList.remove('hidden');
+}
+
+function RequestAssistance() {
+
+}
+
+function GoLogin() {
+    window.location.href = "login.html";
+}
+
+function GoChino() {
+    window.location.href = "EPhomelistChino.html";
+}
+
+function GoButter() {
+    window.location.href = "EPhomelistButter.html";
+}
