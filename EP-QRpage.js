f82d17b3f63f8a5522fcca421c85280b2cbe2c3a
jasroop-singh <113493289+jasroop-singh@users.noreply.github.com>
2023-07-24 00:15:11 -0700

    Update EP-QRpage.js
    dropdown functionality

--- EP-QRpage.js
+++ EP-QRpage.js
@@ -1,9 +1,21 @@
 function RequestBevCart() {
-    var chino_btn = document.getElementById('chino');
-    var butter_btn = document.getElementById('butter');
+    var select_id = document.getElementById('select-id');
+    var go_btn = document.getElementById('go-btn');
 
-    chino_btn.classList.remove('hidden');
-    butter_btn.classList.remove('hidden');
+    select_id.classList.remove('hidden');
+    go_btn.classList.remove('hidden');
+}
+
+function GoCourse() {
+    var dropdown = document.getElementById("select-id");
+    var selectedCourse = dropdown.options[dropdown.selectedIndex].value;
+
+    if(selectedCourse == 1) {
+        window.location.href = "EPhomelistChino.html";
+    }
+    if(selectedCourse == 2) {
+        window.location.href = "EPhomelistButter.html";
+    }
 }
 
 function RequestAssistance() {
@@ -12,12 +24,4 @@ function RequestAssistance() {
 
 function GoLogin() {
     window.location.href = "login.html";
-}
-
-function GoChino() {
-    window.location.href = "EPhomelistChino.html";
-}
-
-function GoButter() {
-    window.location.href = "EPhomelistButter.html";
 }
