64d3435a557e93a12e7f741aa2b8f7e8674da4b6
Jonathan Marquez <45045020+AshianBoii@users.noreply.github.com>
2023-08-07 18:10:05 -0700

    Update EP-QRpage.js

--- EP-QRpage.js
+++ EP-QRpage.js
@@ -11,21 +11,31 @@ function GoCourse() {
     var selectedCourse = dropdown.options[dropdown.selectedIndex].value;
 
     if(selectedCourse == 1) {
+        window.location.href = "EPhomelistChino.html";
+    }
+    if(selectedCourse == 2) {
+        window.location.href = "EPhomelistButter.html";
+    }
+}
+
+function RequestAssistance() {
+    var select_id = document.getElementById('select');
+    var go_btn = document.getElementById('go');
+
+    select_id.classList.remove('button');
+    go_btn.classList.remove('button');
+}
+function Go() {
+    var dropdown = document.getElementById("select");
+    var selectedCourse = dropdown.options[dropdown.selectedIndex].value;
+
+    if(selectedCourse == 1) {
         window.location.href = "EPmaintenanceListChino.html";
     }
     if(selectedCourse == 2) {
         window.location.href = "EPmaintenanceListButter.html";
     }
 }
-
-function RequestAssistance() {
-    var select_id = document.getElementById('select');
-    var go_btn = document.getElementById('go-btn');
-
-    select_id.classList.remove('hidden');
-    go_btn.classList.remove('hidden');
-}
-
 
 function GoLogin() {
     window.location.href = "login.html";
